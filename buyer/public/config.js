45cce3def74cc33048e5e71cd6aa01bdcc648245
yue.yuan1 <yue.yuan@mobilemd.cn>
2023-03-13 22:49:13 +0800

    修改镜像

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,15 +3,19 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "https://master.cloud.com:30890",
+    buyer: "https://master.cloud.com:30888",
+    seller: "https://master.cloud.com:30889",
+    manager: "https://master.cloud.com:30887",
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "https://master.cloud.com:30890",
+    buyer: "https://master.cloud.com:30888",
+    seller: "https://master.cloud.com:30889",
+    manager: "https://master.cloud.com:30887",
+    // common: "https://common-api.pickmall.cn",
+    // buyer: "https://buyer-api.pickmall.cn",
+    // seller: "https://store-api.pickmall.cn",
+    // manager: "https://admin-api.pickmall.cn",
   },
 };
--- manager/public/config.js
+++ manager/public/config.js
@@ -3,17 +3,21 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "https://master.cloud.com:30890",
+      buyer: "https://master.cloud.com:30888",
+      seller: "https://master.cloud.com:30889",
+      manager: "https://master.cloud.com:30887",
     },
-    API_PROD: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
-    },
+  API_PROD: {
+    common: "https://master.cloud.com:30890",
+    buyer: "https://master.cloud.com:30888",
+    seller: "https://master.cloud.com:30889",
+    manager: "https://master.cloud.com:30887",
+    // common: "https://common-api.pickmall.cn",
+    // buyer: "https://buyer-api.pickmall.cn",
+    // seller: "https://store-api.pickmall.cn",
+    // manager: "https://admin-api.pickmall.cn",
+  },
     /**
      * @description // 跳转买家端地址 pc端
      */
