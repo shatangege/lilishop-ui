3c66cddab3222b53e5471c54cf163d25afdea9b9
yue.yuan1 <yue.yuan@mobilemd.cn>
2023-03-14 00:19:28 +0800

    修改镜像

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,4 @@
-docker push registry.cn-beijing.aliyuncs.com/lili-images/buyer-ui:4.2.4.1
-docker push registry.cn-beijing.aliyuncs.com/lili-images/wap-ui:4.2.4.1
-docker push registry.cn-beijing.aliyuncs.com/lili-images/seller-ui:4.2.4.1
-docker push registry.cn-beijing.aliyuncs.com/lili-images/manager-ui:4.2.4.1
-
+docker push registry.cloud.com:5000/lili-images/buyer-ui:4.2.4.1
+docker push registry.cloud.com:5000/lili-images/wap-ui:4.2.4.1
+docker push registry.cloud.com:5000/lili-images/seller-ui:4.2.4.1
+docker push registry.cloud.com:5000/lili-images/manager-ui:4.2.4.1
