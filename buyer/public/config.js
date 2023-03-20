798283bd21af46bd65de1a621007adfda5b1c6be
yue.yuan1 <yue.yuan@mobilemd.cn>
2023-03-20 18:14:04 +0800

    修改镜像

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,16 +3,16 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://master.cloud.com:30890",
-    buyer: "https://master.cloud.com:30888",
-    seller: "https://master.cloud.com:30889",
-    manager: "https://master.cloud.com:30887",
+    common: "https://common-api.cloud.com",
+    buyer: "https://buyer-api.cloud.com",
+    seller: "https://seller-api.cloud.com",
+    manager: "https://manager-api.cloud.com",
   },
   API_PROD: {
-    common: "https://master.cloud.com:30890",
-    buyer: "https://master.cloud.com:30888",
-    seller: "https://master.cloud.com:30889",
-    manager: "https://master.cloud.com:30887",
+    common: "https://common-api.cloud.com",
+    buyer: "https://buyer-api.cloud.com",
+    seller: "https://seller-api.cloud.com",
+    manager: "https://manager-api.cloud.com",
     // common: "https://common-api.pickmall.cn",
     // buyer: "https://buyer-api.pickmall.cn",
     // seller: "https://store-api.pickmall.cn",
