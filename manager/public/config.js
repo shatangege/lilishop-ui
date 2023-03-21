351719e95607d60f20c63030224809b5a3312a77
yue.yuan1 <yue.yuan@mobilemd.cn>
2023-03-21 21:52:10 +0800

    修改镜像

--- manager/public/config.js
+++ manager/public/config.js
@@ -3,16 +3,16 @@ var BASE = {
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.cloud.com",
-      buyer: "https://buyer-api.cloud.com",
-      seller: "https://seller-api.cloud.com",
-      manager: "https://manager-api.cloud.com",
+      common: "https://ggznkj.cloud.com/common-api",
+      buyer: "https://ggznkj.cloud.com/buyer-api",
+      seller: "https://ggznkj.cloud.com/seller-api",
+      manager: "https://ggznkj.cloud.com/manager-api",
     },
   API_PROD: {
-    common: "https://common-api.cloud.com",
-    buyer: "https://buyer-api.cloud.com",
-    seller: "https://seller-api.cloud.com",
-    manager: "https://manager-api.cloud.com",
+    common: "https://ggznkj.cloud.com/common-api",
+    buyer: "https://ggznkj.cloud.com/buyer-api",
+    seller: "https://ggznkj.cloud.com/seller-api",
+    manager: "https://ggznkj.cloud.com/manager-api",
     // common: "https://common-api.pickmall.cn",
     // buyer: "https://buyer-api.pickmall.cn",
     // seller: "https://store-api.pickmall.cn",
--- seller/public/config.js
+++ seller/public/config.js
@@ -3,16 +3,16 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.cloud.com",
-    buyer: "https://buyer-api.cloud.com",
-    seller: "https://seller-api.cloud.com",
-    manager: "https://manager-api.cloud.com",
+    common: "https://ggznkj.cloud.com/common-api",
+    buyer: "https://ggznkj.cloud.com/buyer-api",
+    seller: "https://ggznkj.cloud.com/seller-api",
+    manager: "https://ggznkj.cloud.com/manager-api",
   },
   API_PROD: {
-    common: "https://common-api.cloud.com",
-    buyer: "https://buyer-api.cloud.com",
-    seller: "https://seller-api.cloud.com",
-    manager: "https://manager-api.cloud.com",
+    common: "https://ggznkj.cloud.com/common-api",
+    buyer: "https://ggznkj.cloud.com/buyer-api",
+    seller: "https://ggznkj.cloud.com/seller-api",
+    manager: "https://ggznkj.cloud.com/manager-api",
   },
   /**
    * @description // 跳转买家端地址 pc端
