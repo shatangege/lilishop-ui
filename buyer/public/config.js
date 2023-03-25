6f70e9362c1d68af2896f234198b48c13ce438c4
yue.yuan1 <yue.yuan@mobilemd.cn>
2023-03-25 09:03:11 +0800

    修改镜像

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -3,19 +3,19 @@ var BASE = {
    * @description api请求基础路径
    */
   API_DEV: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
-    // common: "http://192.168.0.105:8890",
-    // buyer: "http://192.168.0.105:8888",
-    // seller: "http://192.168.0.105:8889",
-    // manager: "http://192.168.0.105:8887"
+    common: "https://ggznkj.cloud.com/common-api",
+    buyer: "https://ggznkj.cloud.com/buyer-api",
+    seller: "https://ggznkj.cloud.com/seller-api",
+    manager: "https://ggznkj.cloud.com/manager-api",
   },
   API_PROD: {
-    common: "https://common-api.pickmall.cn",
-    buyer: "https://buyer-api.pickmall.cn",
-    seller: "https://store-api.pickmall.cn",
-    manager: "https://admin-api.pickmall.cn"
+    common: "https://ggznkj.cloud.com/common-api",
+    buyer: "https://ggznkj.cloud.com/buyer-api",
+    seller: "https://ggznkj.cloud.com/seller-api",
+    manager: "https://ggznkj.cloud.com/manager-api",
+    // common: "https://common-api.pickmall.cn",
+    // buyer: "https://buyer-api.pickmall.cn",
+    // seller: "https://store-api.pickmall.cn",
+    // manager: "https://admin-api.pickmall.cn",
   },
 };
